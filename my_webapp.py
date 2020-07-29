ab6df60702575bd53d1116ee988738b4bf043ead
yuwen0107 <a28v021@gmail.com>
2020-07-29 18:26:04 +0800

    First Commit

--- my_webapp.py
+++ my_webapp.py
@@ -1,20 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app) #建立bootstrap物件
 
 @app.route('/')
+def show_resume():
+    return render_template('resume.html')
+
+@app.route('/index')
 def my_index():
     return render_template('index.html')
 
-@app.route('/new')
+@app.route('/index/new')
 def new_data():
     return render_template('new.html')
 
-@app.route('/new/third')
+@app.route('/index/new/third')
 def third_job():
     return render_template('third.html')
+
+
 
 @app.errorhandler(404)  #錯誤處理(找不到頁面)
 def page_not_found(e):
